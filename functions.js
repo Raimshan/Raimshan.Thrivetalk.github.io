3df36f61365ebf65c06a3056716c7d4468c4649e
Raimshan <77324980+Raimshan@users.noreply.github.com>
2021-01-15 23:23:15 +0600

    hnh

---
+++ functions.js
@@ -0,0 +1,26 @@
+$(function() {
+   $(".linkfirst>a").hover(function() {
+   	    $(this).css("border-bottom","2px solid #22356F");
+   });
+   $(".linkfirst>a").mouseout(function(event) {
+   	    $(this).css("border-style","none");
+   });
+   $(".linksecond>a").hover(function() {
+   	    $(this).css("border-bottom","2px solid #22356F");
+   });
+   $(".linksecond>a").mouseout(function(event) {
+   	    $(this).css("border-style","none");
+   });
+   $(".linkthird>a").hover(function() {
+   	    $(this).css("border-bottom","2px solid #22356F");
+   });
+   $(".linkthird>a").mouseout(function(event) {
+   	    $(this).css("border-style","none");
+   });
+   $(".linkfourth>a").hover(function() {
+   	    $(this).css("border-bottom","2px solid #22356F");
+   });
+   $(".linkfourth>a").mouseout(function(event) {
+   	    $(this).css("border-style","none");
+   });
+});
